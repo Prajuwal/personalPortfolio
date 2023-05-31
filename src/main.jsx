c566ed0bc71d70cd3663c354a995cf652a2c8f9c
Prajuwal <prajuwalsingh@gmail.com>
2023-05-31 23:40:35 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello world!</h1>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
