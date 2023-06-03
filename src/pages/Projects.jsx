2b23edcd4dc6ec8b40bf18a29db676fb189485ee
Prajuwal <prajuwalsingh@gmail.com>
2023-06-03 16:56:32 +0530

    project on clicked event pending

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,20 @@
+import ProjectItem from "../components/ProjectItem";
+import { ProjectList } from "../helpers/ProjectList";
+
+import "../styles/Projects.css";
+
 const Projects = () => {
-  return <h1>Projects</h1>;
+  return (
+    <div className="projects">
+      <h1> My Personal Projects</h1>
+      <div className="projectList">
+        {ProjectList.map((project, idx) => {
+          return (
+            <ProjectItem id={idx} name={project.name} image={project.image} />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Projects;
